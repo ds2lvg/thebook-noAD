9c39fb2052905c596fd21f3e39dbaf5e291295e3
HomeReinkaos <ds2lvg@gmail.com>
2020-01-20 07:02:52 +0900

    광고가리기 함수로 리팩토링

--- action.js
+++ action.js
@@ -1,9 +1,18 @@
-// 실행할 스크립트
-document.querySelector(".google-auto-placed").style.display="none";
-document.querySelectorAll(".thebook-section--center")[0].style.display="none";
-document.querySelectorAll(".mdl-layout__header-row")[1].style.display="none";
-document.querySelectorAll(".mdl-grid")[2].style.display="none";
-document.querySelectorAll(".mdl-grid")[5].style.display="none";
-document.querySelector(".google-auto-placed").style.display="none";
-document.querySelectorAll(".thebook-vsp").forEach(v => v.style.display="none");
-document.querySelector("footer").style.display="none";
+// 광고 가리기
+(function hideDom() {
+ arguments[0].forEach(dom => {
+  if(dom.length === undefined) dom.style.display="none";
+  else dom.forEach(v => v.style.display="none");
+ });
+})([
+  document.querySelector(".google-auto-placed"),
+  document.querySelectorAll(".thebook-section--center")[0],
+  document.querySelectorAll(".mdl-layout__header-row")[1],
+  document.querySelectorAll(".mdl-grid")[2],
+  document.querySelectorAll(".mdl-grid")[5],
+  document.querySelector(".google-auto-placed"),
+  document.querySelectorAll(".thebook-vsp"),
+]);
+
+// 메뉴 상위로 이동
+const menu = document.querySelectorAll('.thebook-section--center')[3].cloneNode();
