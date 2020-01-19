07bb26ae295ac3aba5b3568484a0c32dacd977c8
HomeReinkaos <ds2lvg@gmail.com>
2020-01-19 21:17:57 +0900

    first commit

---
+++ action.js
@@ -0,0 +1,9 @@
+// 실행할 스크립트
+document.querySelector(".google-auto-placed").style.display="none";
+document.querySelectorAll(".thebook-section--center")[0].style.display="none";
+document.querySelectorAll(".mdl-layout__header-row")[1].style.display="none";
+document.querySelectorAll(".mdl-grid")[2].style.display="none";
+document.querySelectorAll(".mdl-grid")[5].style.display="none";
+document.querySelector(".google-auto-placed").style.display="none";
+document.querySelectorAll(".thebook-vsp").forEach(v => v.style.display="none");
+document.querySelector("footer").style.display="none";
