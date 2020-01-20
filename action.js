fba6b389a687c2b897a4d0b522b98839b120b7d2
HomeReinkaos <ds2lvg@gmail.com>
2020-01-20 22:35:29 +0900

    다른 웹페이지 구글 광고 가릴수 있게 변경,  thebook 메뉴 오른쪽으로 이동

--- action.js
+++ action.js
@@ -1,9 +1,11 @@
 // 광고 가리기
 (function hideDom() {
- arguments[0].forEach(dom => {
-  if(dom.length === undefined) dom.style.display="none";
-  else dom.forEach(v => v.style.display="none");
- });
+  arguments[0].forEach(dom => {
+    if(dom){
+      if(dom.length === undefined) dom.style.display="none";
+      else dom.forEach(v => v.style.display="none");
+    }
+  });
 })([
   document.querySelector(".google-auto-placed"),
   document.querySelectorAll(".thebook-section--center")[0],
@@ -12,10 +14,27 @@
   document.querySelectorAll(".mdl-grid")[5],
   document.querySelector(".google-auto-placed"),
   document.querySelectorAll(".thebook-vsp"),
+  document.querySelectorAll('ins'),
 ]);
 
-// 메뉴 상위로 이동
-const menu = document.querySelectorAll('.thebook-section--center')[3];
-// menu.style.margin="0px";
-// munu.querySelector('.mdl-layout-spacer').style.flexGrow = "0";
-document.getElementById("thebook-main").insertAdjacentElement('afterbegin', menu);
+const url = document.location.href.split("/")[2]
+if(url === "thebook.io") {
+  const menu = document.querySelectorAll('.thebook-section--center')[3];
+  // 메뉴 상위로 이동
+  document.getElementById("thebook-main").insertAdjacentElement('afterbegin', menu);
+  // 스타일 삽입
+  const css = `
+  #thebook-main > section:first-child{max-width: 100%; margin-right: 10%;}
+  .mdl-layout-spacer{flex-grow: 0;}
+  .gas > .mdl-grid.mdl-grid--no-spacing{justify-content: flex-end;}
+  `
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet){
+      style.styleSheet.cssText = css;
+  } else {
+      style.appendChild(document.createTextNode(css));
+  }
+  head.appendChild(style);
+}
